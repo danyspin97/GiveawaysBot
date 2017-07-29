9129cc12f405ad59c4657e8c7704a7a691126010
Dom Corvasce <dom.corvasce@yandex.com>
2017-07-29 12:24:31 +0200

    Isolate constants into a dedicated file

---
+++ src/giveaway/constants.php
@@ -0,0 +1,44 @@
+<?php
+define('NEWLINE','
+');
+
+define('NO_STATUS', -1);
+define('MENU', 0);
+define('JOINING', 1);
+define('SELECTING_TYPE', 2);
+define('ENTERING_TITLE', 3);
+define('ENTERING_HASHTAG', 4);
+define('ENTERING_MAX', 5);
+define('ENTERING_SHARE', 6);
+define('ENTERING_DESCRIPTION', 7);
+define('ENTERING_DATE', 8);
+define('GIVEAWAY_SUMMARY', 9);
+define('GIVEAWAY_EDIT_TITLE', 10);
+define('GIVEAWAY_EDIT_HASHTAG', 11);
+define('GIVEAWAY_EDIT_MAX', 12);
+define('GIVEAWAY_EDIT_DESCRIPTION', 13);
+define('GIVEAWAY_EDIT_DATE', 14);
+define('ENTERING_PRIZE_NAME', 15);
+define('ENTERING_PRIZE_TYPE', 16);
+define('ENTERING_PRIZE_VALUE', 17);
+define('ENTERING_PRIZE_CURRENCY', 18);
+define('ENTERING_PRIZE_KEY', 19);
+define('PRIZE_SUMMARY', 20);
+define('PRIZE_DETAIL', 21);
+define('PRIZE_DETAIL_EDIT_NAME', 22);
+define('PRIZE_DETAIL_EDIT_TYPE', 23);
+define('PRIZE_DETAIL_EDIT_VALUE', 24);
+define('PRIZE_DETAIL_EDIT_CURRENCY', 25);
+define('SHOW_GIVEAWAY_DETAILS', 26);
+define('JOIN_HASHTAG_PROMPT', 27);
+define('JOINED', 28);
+define('GV_NOT_VALID', 29);
+define('SHOW_ALL', 30);
+define('LANGUAGE', 31);
+define('GIVEAWAY_CANCEL_PROMPT', 32);
+define('PRIZE_CANCEL_PROMPT', 33);
+define('SHOW_GIVEAWAY_LIST', 34);
+define('SHOW_PRIZES', 35);
+define('OBJECT_PER_LIST', 3);
+define('CURRENCY', '€$₽');
+
